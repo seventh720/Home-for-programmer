d59f21a935095b9e9bd7e0b9103a0b0e4a9c0037
“seventh720” <1791303902@qq.com>
2023-11-01 12:49:43 +0800

    addition

---
+++ practical2/addition.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;  //need for input
+public class addition{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter start number: ");
+        int start = input.nextInt();
+        System.out.print("Enter end number: ");
+        int end = input.nextInt();
+        int sum = 0;
+        if(start < end){
+            sum = start;
+            do{start = start+1;
+                sum += start;
+            }while(start < end);
+            System.out.printf("Result is: %d", sum);
+        }
+        else if (start == end){
+            sum = start;
+            System.out.printf("Result is: %d", sum) ;
+        }
+        else{
+            System.out.println("Wrong input");
+        }
+    }
+}
+
